074b6652f442bfef9748e9dae2c31fc774217a7e
Amanbairagi17 <amanbairagi741@gmail.com>
2026-03-06 14:31:50 +0530

    [Aman] : added add contact method

---
+++ src/main/java/com/example/demo/AddressBookApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.example.demo.service.AddressBook;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookApplication.class, args);
+		System.out.println("Welcome to Address Book Program");
+
+		AddressBook addressBook = new AddressBook();
+
+		addressBook.addContact();
+	}
+
+}
