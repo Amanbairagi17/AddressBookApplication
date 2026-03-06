21cd846d42c4a39b2deba73f496e220608911a1a
Amanbairagi17 <amanbairagi741@gmail.com>
2026-03-06 15:19:17 +0530

    [Aman] : added method to add multiple contact

--- src/main/java/com/example/demo/service/AddressBook.java
+++ src/main/java/com/example/demo/service/AddressBook.java
@@ -94,4 +94,14 @@ public class AddressBook {
 
         System.out.println("Contact not found.");
     }
+
+    public void displayContacts() {
+        if (contactList.isEmpty()) {
+            System.out.println("No contacts available.");
+            return;
+        }
+        for (Contact contact : contactList) {
+            System.out.println(contact);
+        }
+    }
 }
